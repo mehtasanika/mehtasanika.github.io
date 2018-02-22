981116dddc2c7ef745c245fdf6e2e81864f34fbf
Sanika <mehtasanika.github.io>
2018-02-21 22:00:52 -0500

    Interaction Studio Interface Final

---
+++ interface/test.js
@@ -0,0 +1,19 @@
+var sound1 = new Audio('wronganswer.mp3');
+var sound2 = new Audio('cheering.wav');
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+
+opt1.addEventListener('click', test);
+opt2.addEventListener('click', test);
+opt3.addEventListener('click', test);
+
+function test() {
+  if (this.className == 'wrong') {
+    console.log('wrong');
+    sound1.play();
+  } 
+  else {
+    console.log('right');
+  }
+}
